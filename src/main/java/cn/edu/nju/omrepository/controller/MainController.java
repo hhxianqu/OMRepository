eaddf0563ab023e37c99744829dc57dfaf57e105
huangxiao <hx36w35@163.com>
2018-07-27 22:30:05 +0800

    demo

--- src/main/java/cn/edu/nju/omrepository/controller/MainController.java
+++ src/main/java/cn/edu/nju/omrepository/controller/MainController.java
@@ -1,13 +1,25 @@
 package cn.edu.nju.omrepository.controller;
 
+import cn.edu.nju.omrepository.service.TempService;
 import de.felixroske.jfxsupport.FXMLController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import javax.annotation.Resource;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 @FXMLController
 public class MainController implements Initializable {
+
+    @Resource
+    private TempService tempService;
+
+    @FXML
+    void onClick(ActionEvent event) {
+        tempService.addProduct();
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
