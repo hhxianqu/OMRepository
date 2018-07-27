ff9004ee21f0f63953bb41ba4d7242e9ba785a70
huangxiao <hx36w35@163.com>
2018-07-27 23:35:18 +0800

    自定义加载动画

---
+++ src/main/java/cn/edu/nju/omrepository/CustomSplash.java
@@ -0,0 +1,22 @@
+package cn.edu.nju.omrepository;
+
+import de.felixroske.jfxsupport.SplashScreen;
+
+public class CustomSplash extends SplashScreen {
+
+    /**
+     * 加载动画路径
+     */
+    @Override
+    public String getImagePath() {
+        return "/image/test.jpg";
+    }
+
+    /**
+     * 是否显示加载动画
+     */
+    @Override
+    public boolean visible() {
+        return super.visible();
+    }
+}
